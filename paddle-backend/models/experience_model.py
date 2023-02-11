5ddeec9f979ca4887ce977eb1b243eb660ed8613
Phillip Tran <phillip.qtr@gmail.com>
2023-02-11 17:09:53 -0600

    pt - add experience(s)

--- paddle-backend/models/experience_model.py
+++ paddle-backend/models/experience_model.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel, Field, EmailStr
 from bson import ObjectId
+from typing import List
 from models.py_object_id import PyObjectId
 
 
 class ExperienceModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str = Field(...)
-    coord: str = Field(...)
+    coord: dict = Field(...)
     address: str = Field(...)
     country: str = Field(...)
+    city: str = Field(...)
     imageUrl: str = Field(...)
     description: str = Field(...)
     date: str = Field(...)
+    time: str = Field(...)
     summary: str = Field(...)
     videoUrl: str = Field(...)
 

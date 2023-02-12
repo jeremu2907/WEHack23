8677f8a60e087fafc93efc88586629223edf856a
Phillip Tran <phillip.qtr@gmail.com>
2023-02-11 23:09:08 -0600

    pt - add nginx and swagger

--- paddle-backend/main.py
+++ paddle-backend/main.py
@@ -2,11 +2,33 @@ import os
 from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from models.experience_model import ExperienceModel
 from typing import List
 import motor.motor_asyncio
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url="/api/openapi.json")
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/api/static", StaticFiles(directory="static"), name="static")
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.paddle
 
@@ -14,6 +36,31 @@ db = client.paddle
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+        return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/api/redoc", include_in_schema=False)
+async def redoc_html():
+        return get_redoc_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - ReDoc",
+            redoc_js_url="/api/static/redoc.standalone.js",
+        )
+
+
+@ app.get("/api/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - Swagger UI",
+            oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+            swagger_js_url="/api/static/swagger-ui-bundle.js",
+            swagger_css_url="/api/static/swagger-ui.css",
+        )
 
 
 @app.get("/api/v1/experience")
@@ -25,7 +72,6 @@ async def get_experience(id: str = None):
 @app.get("/api/v1/experiences")
 async def get_experiences(name: str = None, city: str = None, country: str = None, groupby: str = 'name'):
     resp_body = []
-    print(country)
     db_args = {}
     if name is not None:
         db_args['name'] = {'$eq': name}

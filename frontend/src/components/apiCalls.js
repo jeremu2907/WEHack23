7ff45b18620648f7a48da1a91da62bff3eec4990
jeremu2907 <jeremu2907@gmail.com>
2023-02-11 22:00:57 -0600

    map

---
+++ frontend/src/components/apiCalls.js
@@ -0,0 +1,12 @@
+export function searchCityCountry(city, country){
+    fetch()
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+
+    })
+}
+
+export function searchByName(place){
+}
